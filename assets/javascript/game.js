4abe7e8f54c18f5d1807c74f0d03892adac73dad
Pamela Vasquez <pamelavasquez94@gmail.com>
2019-11-17 15:45:57 -0500

    made changes to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,10 @@ var princessLeia = {
         var damage = this.attackPower * n;
         if (player.hp > 0) {
             player.hp -= damage;
-            this.hp -= player.counterAttack
+            this.hp -= player.counterAttack;
         }
         if (player.hp <= 0) {
             console.log(`${player.name} is eliminated`);
-            alert(`You have eliminated ${player.name}!`)
             player.eliminate();
         }
         if (this.hp <= 0) {
@@ -20,14 +19,15 @@ var princessLeia = {
             alert('You Lost!')
             $('#attack').prop('disabled', true);
         }
-        console.log(`${this.name} attack power is ${damage}`);
-        console.log(`${player.name} attack power is ${player.counterAttack}`);
+        $('#selectedPlayerDamage').text(`${this.name} attack power is ${damage}`);
+        $('#enemiesDamage').text(`${player.name} attack power is ${player.counterAttack}`);
         console.log(player.hp);
         console.log(this.hp);
 
     },
     eliminate: function () {
-        $("#princessLeia").css("display", "none")
+        $(".defender").css("display", "none");
+        $('#winlosemsg').append(`<p>You have eliminated Princess Leia!</p>`)
     }
 
 }
@@ -35,7 +35,7 @@ var hanSolo = {
     name: "Han Solo",
     hp: 170,
     attackPower: 10,
-    counterAttack: 25,
+    counterAttack: 18,
     attack: function (player) {
         var damage = this.attackPower * n;
         if (player.hp > 0) {
@@ -44,7 +44,6 @@ var hanSolo = {
         }
         if (player.hp <= 0) {
             console.log(`${player.name} is eliminated`);
-            alert(`You have eliminated ${player.name}!`)
             player.eliminate();
         }
         if (this.hp <= 0) {
@@ -52,14 +51,15 @@ var hanSolo = {
             alert('You Lost!')
             $('#attack').prop('disabled', true);
         }
-        console.log(`${this.name} attack power is ${damage}`);
-        console.log(`${player.name} attack power is ${player.counterAttack}`);
+        $('#selectedPlayerDamage').text(`${this.name} attack power is ${damage}`);
+        $('#enemiesDamage').text(`${player.name} attack power is ${player.counterAttack}`);
         console.log(player.hp);
         console.log(this.hp);
 
     },
     eliminate: function () {
-        $("#hanSolo").css("display", "none")
+        $(".defender").css("display", "none");
+        $('#winlosemsg').append(`<p>You have eliminated Han Solo!</p>`)
     }
 
 }
@@ -76,7 +76,6 @@ var chewy = {
         }
         if (player.hp <= 0) {
             console.log(`${player.name} is eliminated`);
-            alert(`You have eliminated ${player.name}!`)
             player.eliminate();
         }
         if (this.hp <= 0) {
@@ -84,14 +83,15 @@ var chewy = {
             alert('You Lost!')
             $('#attack').prop('disabled', true);
         }
-        console.log(`${this.name} attack power is ${damage}`);
-        console.log(`${player.name} attack power is ${player.counterAttack}`);
+        $('#selectedPlayerDamage').text(`${this.name} attack power is ${damage}`);
+        $('#enemiesDamage').text(`${player.name} attack power is ${player.counterAttack}`);
         console.log(player.hp);
         console.log(this.hp);
 
     },
     eliminate: function () {
-        $("#chewy").css("display", "none")
+        $(".defender").css("display", "none");
+        $('#winlosemsg').append(`<p>You have eliminated Chewy!</p>`)
     }
 
 }
@@ -99,7 +99,7 @@ var yoda = {
     name: "Yoda",
     hp: 140,
     attackPower: 7,
-    counterAttack: 30,
+    counterAttack: 12,
     attack: function (player) {
         var damage = this.attackPower * n;
         if (player.hp > 0) {
@@ -108,7 +108,6 @@ var yoda = {
         }
         if (player.hp <= 0) {
             console.log(`${player.name} is eliminated`);
-            alert(`You have eliminated ${player.name}!`)
             player.eliminate();
         }
         if (this.hp <= 0) {
@@ -116,14 +115,16 @@ var yoda = {
             alert('You Lost!')
             $('#attack').prop('disabled', true);
         }
-        console.log(`${this.name} attack power is ${damage}`);
-        console.log(`${player.name} attack power is ${player.counterAttack}`);
+        $('#selectedPlayerDamage').text(`${this.name} attack power is ${damage}`);
+        $('#enemiesDamage').text(`${player.name} attack power is ${player.counterAttack}`);
         console.log(player.hp);
         console.log(this.hp);
 
     },
     eliminate: function () {
-        $("#yoda").css("display", "none")
+        $(".defender").css("display", "none");
+        $('#winlosemsg').append(`<p>You have eliminated Yoda!</p>`)
+
     }
 
 }
@@ -134,54 +135,230 @@ function increment() {
     return n;
 }
 
-function playGame1() {
-    hanSolo.attack(yoda);
-    increment();
-}
-
-function playGame2() {
-    hanSolo.attack(chewy);
-    increment();
-}
-
-function playGame3() {
-    hanSolo.attack(princessLeia);
-    increment();
-}
-
-$("#attack").click(function () {
-    switch (yoda.hp > 0) {
-        case (chewy.hp <= 0):
-            playGame2();
-            break;
-        case (princessLeia.hp <= 0):
-            playGame3();
-            break;
-        default:
-            playGame1();
-            break;
+function leiaVSsolo() {
+    if (hanSolo.hp > 0) {
+        princessLeia.attack(hanSolo);
+        increment();
     }
-    // if (yoda.hp <= 0) {
-    //     playGame2();
-    //     // $.when(playGame2()).then(playGame3());
+}
 
-    //     if (chewy.hp <= 0) {
-    //         playGame3();
-    //     }
-    // } else {
-    //     playGame1();
-    // }
-    // if (yoda.hp <= 0 && chewy.hp <= 0 && princessLeia.hp <= 0) {
-    //     alert('You WIN!')
-    // }
+function leiaVSchewy() {
+    if (chewy.hp > 0) {
+        princessLeia.attack(chewy);
+        increment();
+    }
+}
 
+function leiaVSyoda() {
+    if (yoda.hp > 0) {
+        princessLeia.attack(yoda);
+        increment();
+    }
+}
+
+function soloVSleia() {
+    if (princessLeia.hp > 0) {
+        hanSolo.attack(princessLeia);
+        increment();
+    }
+}
+
+function soloVSchewy() {
+    if (chewy.hp > 0) {
+        hanSolo.attack(chewy);
+        increment();
+    }
+}
+
+function soloVSyoda() {
+    if (yoda.hp > 0) {
+        hanSolo.attack(yoda);
+        increment();
+    }
+}
+
+function chewyVSleia() {
+    if (princessLeia.hp > 0) {
+        chewy.attack(princessLeia);
+        increment();
+    }
+}
+
+function chewyVSsolo() {
+    if (hanSolo.hp > 0) {
+        chewy.attack(hanSolo);
+        increment();
+    }
+}
+
+function chewyVSyoda() {
+    if (yoda.hp > 0) {
+        chewy.attack(yoda);
+        increment();
+    }
+}
+
+function yodaVSleia() {
+    if (princessLeia.hp > 0) {
+        yoda.attack(princessLeia);
+        increment();
+    }
+}
+
+function yodaVSsolo() {
+    if (hanSolo.hp > 0) {
+        yoda.attack(hanSolo);
+        increment();
+    }
+}
+
+function yodaVSchewy() {
+    if (chew.hp > 0) {
+        yoda.attack(chewy);
+        increment();
+    }
+}
+
+$('.princessLeiaHP').prepend(`<p>${princessLeia.hp}</p>`).addClass('hp')
+$('.hanSoloHP').append(`<p>${hanSolo.hp}</p>`).addClass('hp')
+$('.chewyHP').append(`<p>${chewy.hp}</p>`).addClass('hp')
+$('.yodaHP').append(`<p>${yoda.hp}</p>`).addClass('hp')
+
+
+
+$('#princessLeia').one("click", function () {
+    $('#princessLeia').appendTo('#selectedCharacterBank').addClass('selectedCharacter')
+    $('#Leia-enemiesToAttack').css('display', 'flex')
+    $('#hanSolo').css('display', 'none')
+    $('#chewy').css('display', 'none')
+    $('#yoda').css('display', 'none')
+
+    $('#hanSolo-Leia-enemies').one("click", function () {
+        $('#hanSolo-Leia-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            leiaVSsolo();
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+        });
+    });
+    $('#chewy-Leia-enemies').one("click", function () {
+        $('#chewy-Leia-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            leiaVSchewy();
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+            $('.chewyHP').text(`${chewy.hp}`)
+        });
+
+    });
+    $('#yoda-Leia-enemies').one("click", function () {
+        $('#yoda-Leia-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            leiaVSyoda();
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+            $('.yodaHP').text(`${yoda.hp}`)
+        });
+    });
 });
 
-$('#princessLeiaHP').append(`${princessLeia.hp}`)
-$('#hanSoloHP').append(`${hanSolo.hp}`)
-$('#chewyHP').append(`${chewy.hp}`)
-$('#yodaHP').append(`${yoda.hp}`)
 
-$('#princessLeia').click(function () {
-    $('#princessLeia').appendTo('#selectedCharacter')
+$('#hanSolo').one("click", function () {
+    $('#hanSolo').appendTo('#selectedCharacterBank').addClass('selectedCharacter')
+    $('#solo-enemiesToAttack').css('display', 'flex')
+    $('#princessLeia').css('display', 'none')
+    $('#chewy').css('display', 'none')
+    $('#yoda').css('display', 'none')
+
+
+    $('#princessLeia-solo-enemies').one("click", function () {
+        $('#princessLeia-solo-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            soloVSleia();
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+        });
+    });
+    $('#yoda-solo-enemies').one("click", function () {
+        $('#yoda-solo-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            soloVSyoda();
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+            $('.yodaHP').text(`${yoda.hp}`)
+        });
+    });
+    $('#chewy-solo-enemies').one("click", function () {
+        $('#chewy-solo-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            soloVSchewy();
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+            $('.chewyHP').text(`${chewy.hp}`)
+        });
+    });
+});
+
+$('#chewy').one("click", function () {
+    $('#chewy').appendTo('#selectedCharacterBank').addClass('selectedCharacter')
+    $('#chewy-enemiesToAttack').css('display', 'flex')
+    $('#hanSolo').css('display', 'none')
+    $('#princessLeia').css('display', 'none')
+    $('#yoda').css('display', 'none')
+
+
+    $('#princessLeia-chewy-enemies').one("click", function () {
+        $('#princessLeia-chewy-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            chewyVSleia();
+            $('.chewyHP').text(`${chewy.hp}`)
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+        });
+    });
+    $('#yoda-chewy-enemies').one("click", function () {
+        $('#yoda-chewy-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            chewyVSyoda();
+            $('.chewyHP').text(`${chewy.hp}`)
+            $('.yodaHP').text(`${yoda.hp}`)
+        });
+    });
+    $('#hanSolo-chewy-enemies').one("click", function () {
+        $('#hanSolo-chewy-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            chewyVSsolo();
+            $('.chewyHP').text(`${chewy.hp}`)
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+        });
+    });
+});
+
+$('#yoda').one("click", function () {
+    $('#yoda').appendTo('#selectedCharacterBank').addClass('selectedCharacter')
+    $('#yoda-enemiesToAttack').css('display', 'flex')
+    $('#hanSolo').css('display', 'none')
+    $('#princessLeia').css('display', 'none')
+    $('#chewy').css('display', 'none')
+
+
+    $('#princessLeia-yoda-enemies').one("click", function () {
+        $('#princessLeia-yoda-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            yodaVSleia();
+            $('.yodaHP').text(`${yoda.hp}`)
+            $('.princessLeiaHP').text(`${princessLeia.hp}`)
+        });
+    });
+    $('#chewy-yoda-enemies').one("click", function () {
+        $('#chewy-yoda-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            yodaVSchewy();
+            $('.yodaHP').text(`${yoda.hp}`)
+            $('.chewyHP').text(`${chewy.hp}`)
+        });
+    });
+    $('#hanSolo-yoda-enemies').one("click", function () {
+        $('#hanSolo-yoda-enemies').appendTo('#oppnent').addClass('defender');
+        $("#attack").click(function () {
+            yodaVSsolo();
+            $('.yodaHP').text(`${yoda.hp}`)
+            $('.hanSoloHP').text(`${hanSolo.hp}`)
+        });
+    });
 });
